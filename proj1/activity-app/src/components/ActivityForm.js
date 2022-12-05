384c59850aee2cb87598137390249d825a7aefff
Felipe R. Tasoniero <frtasoniero@gmail.com>
2022-12-04 22:51:13 -0300

    frontend(03): edit activity

--- proj1/activity-app/src/components/ActivityForm.js
+++ proj1/activity-app/src/components/ActivityForm.js
@@ -1,41 +1,118 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+const initialActivity = {
+  id: 0,
+  title: "",
+  priority: "0",
+  description: ""
+};
 
 export default function ActivityForm(props) {
+  const [activity, setActivity] = useState(currentActivity());
+
+  useEffect(
+    () => {
+      if (props.selectedActivity.id !== 0) setActivity(props.selectedActivity);
+    },
+    [props.selectedActivity]
+  );
+
+  const inputTextHandler = (e) => {
+    const { name, value } = e.target;
+    setActivity({ ...activity, [name]: value });
+  };
+
+  const cancelEditHandler = (e) => {
+    e.preventDefault();
+
+    props.cancelActivity();
+
+    setActivity(initialActivity);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (props.selectedActivity.id !== 0) props.updateActivity(activity);
+    else props.addActivity(activity);
+
+    setActivity(initialActivity);
+  };
+
+  function currentActivity() {
+    if (props.selectedActivity.id !== 0) return props.selectedActivity;
+    else return initialActivity;
+  }
+
   return (
-    <form className="row g-3 mt-2">
+    <>
+    <h1>Activity {activity.id !== 0 ? activity.id : ''}</h1>
+    <form className="row g-3 mt-2" onSubmit={handleSubmit}>
       <div className="col-md-6">
-        <label className="form-label">Id</label>
+        <label className="form-label">Title</label>
         <input
-          disabled
           type="text"
           className="form-control"
-          id="id"
-          value={props.currentId}
+          id="title"
+          name="title"
+          value={activity.title}
+          onChange={inputTextHandler}
         />
       </div>
       <div className="col-md-6">
         <label className="form-label">Priority</label>
-        <select id="priority" className="form-select">
+        <select
+          id="priority"
+          className="form-select"
+          name="priority"
+          value={activity.priority}
+          onChange={inputTextHandler}
+        >
           <option defaultValue="0">Choose...</option>
           <option value="1">Low</option>
           <option value="2">Normal</option>
           <option value="3">High</option>
         </select>
       </div>
-      <div className="col-md-6">
-        <label className="form-label">Title</label>
-        <input type="text" className="form-control" id="title" />
-      </div>
-      <div className="col-md-6">
+      <div className="col-md-12">
         <label className="form-label">Description</label>
-        <input type="text" className="form-control" id="description" />
+        <textarea
+          type="text"
+          className="form-control"
+          id="description"
+          name="description"
+          value={activity.description}
+          onChange={inputTextHandler}
+        />
+        <hr />
       </div>
       <div className="col-12">
-        <button className="btn btn-outline-primary" onClick={props.addAcitivity}>
-          + Activity
-        </button>
+        {
+          activity.id === 0 ?
+          <button
+            className="btn btn-outline-primary"
+            type="submit"
+          >
+            <i className="fas fa-plus me-2"></i>Activity
+          </button>
+          :
+          <>
+            <button
+                className="btn btn-outline-success me-2"
+                type="submit"
+            >
+                <i className="fas fa-plus me-2"></i>Save
+            </button>
+            <button
+                className="btn btn-outline-warning"
+                onClick={cancelEditHandler}
+            >
+                <i className="fas fa-xmark me-2"></i>Cancel
+            </button>
+          </>
+        }
       </div>
-      <hr />
     </form>
+    </>
   );
 }
