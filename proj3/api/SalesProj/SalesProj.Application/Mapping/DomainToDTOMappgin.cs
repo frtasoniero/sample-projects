5e39d4ad70af77fba3327b0181c1076b5bb21d29
Felipe R. Tasoniero <frtasoniero@gmail.com>
2022-12-22 20:20:48 -0300

    feat: adding DTOs and Mapping

---
+++ proj3/api/SalesProj/SalesProj.Application/Mapping/DomainToDTOMappgin.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SalesProj.Application.DTOs;
+using SalesProj.Domain.Entities;
+
+namespace SalesProj.Application.Mapping
+{
+    public class DomainToDTOMappgin : Profile
+    {
+        public DomainToDTOMappgin() 
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
