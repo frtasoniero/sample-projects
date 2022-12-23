9025f5fed339697f25989e7d6c5eb219575706ae
Felipe R. Tasoniero <frtasoniero@gmail.com>
2022-12-23 01:53:19 -0300

    feat: adding queries and commands for products

---
+++ proj3/api/SalesProj/SalesProj.Application/Products/Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SalesProj.Application.Products.Queries;
+using SalesProj.Domain.Entities;
+using SalesProj.Domain.Interfaces;
+
+namespace SalesProj.Application.Products.Handlers
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        private IProductRepository _productRepository;
+
+        public GetProductByIdHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetProductByIdAsync(request.Id);
+        }
+    }
+}
