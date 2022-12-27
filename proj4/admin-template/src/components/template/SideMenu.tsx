45b60e2dcbe8ec7a6984706310a28e3467f80262
Felipe R. Tasoniero <frtasoniero@gmail.com>
2022-12-27 13:05:14 -0300

    feat: adding logo

--- proj4/admin-template/src/components/template/SideMenu.tsx
+++ proj4/admin-template/src/components/template/SideMenu.tsx
@@ -1,9 +1,16 @@
 import { DataIcon, HomeIcon, SettingsIcon } from "../icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function SideMenu() {
     return (
         <aside>
+            <div className={`
+            h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800
+            flex flex-col items-center justify-center
+            `}>
+                <Logo></Logo>
+            </div>
             <ul>
                 <MenuItem url="/" text="Home" icon={HomeIcon}></MenuItem>
                 <MenuItem url="/settings" text="Settings" icon={SettingsIcon}></MenuItem>
