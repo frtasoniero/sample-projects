9194e4ab42eb43fae75fdde7e7004e9fb6313d3a
Felipe R. Tasoniero <frtasoniero@gmail.com>
2022-12-27 13:26:40 -0300

    feat: adding logout button and changing side menu layout

--- proj4/admin-template/src/components/template/SideMenu.tsx
+++ proj4/admin-template/src/components/template/SideMenu.tsx
@@ -1,20 +1,27 @@
-import { DataIcon, HomeIcon, SettingsIcon } from "../icons";
+import { DataIcon, HomeIcon, LogoutIcon, SettingsIcon } from "../icons";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function SideMenu() {
     return (
-        <aside>
+        <aside className={`flex flex-col`}>
             <div className={`
             h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800
             flex flex-col items-center justify-center
             `}>
                 <Logo></Logo>
             </div>
-            <ul>
+            <ul className={`flex-grow`}>
                 <MenuItem url="/" text="Home" icon={HomeIcon}></MenuItem>
                 <MenuItem url="/settings" text="Settings" icon={SettingsIcon}></MenuItem>
                 <MenuItem url="/data" text="Data" icon={DataIcon}></MenuItem>
+            </ul>
+            <ul className={``}>
+                <MenuItem 
+                    text="Logout" icon={LogoutIcon}
+                    onClick={() => console.log("Logout...")}
+                    className={`text-red-600 hover:bg-red-400 hover:text-white`}
+                ></MenuItem>
             </ul>
         </aside>
     )
