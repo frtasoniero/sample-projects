ea2dfec5400948c2a645f6cdffa393030c008bff
Felipe R. Tasoniero <frtasoniero@gmail.com>
2022-12-27 15:22:28 -0300

    feat: change theme button on settings

--- proj4/admin-template/src/pages/settings.tsx
+++ proj4/admin-template/src/pages/settings.tsx
@@ -1,10 +1,13 @@
 import Layout from "../components/template/Layout";
+import useAppData from "../data/hook/useAppData";
 
 export default function Settings() {
+  const {changeTheme} = useAppData()
+
   return (
     <>
       <Layout title="Settings Page" subtitle="Your settings can be modified here!">
-        <h3>Settings</h3>
+        <button onClick={changeTheme}>Change Theme</button>
       </Layout>
     </>
   )
